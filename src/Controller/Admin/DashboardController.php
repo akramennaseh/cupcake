855168e6335976da1c395a7a8878af724999220c
akramennaseh <akramennaseh01@gmail.com>
2022-01-07 00:58:47 +0100

    first commit

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Auteur;
+use App\Entity\Genre;
+use App\Entity\Livre;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Bookstore');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Livres', 'fas fa-book', Livre::class);
+        yield MenuItem::linkToCrud('Auteurs', 'fas fa-book', Auteur::class);
+        yield MenuItem::linkToCrud('Genres', 'fas fa-book', Genre::class);
+   
+    }
+}
